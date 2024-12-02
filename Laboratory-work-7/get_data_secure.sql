91bf82e13d8613298a4142cf0424e3a93630c09a
Artem Sultanov <79450613+ArtemSultanov@users.noreply.github.com>
2024-12-02 15:07:49 +0200

    Update get_data_secure.sql

--- Laboratory-work-7/get_data_secure.sql
+++ Laboratory-work-7/get_data_secure.sql
@@ -1,1 +1,7 @@
-
+CREATE OR REPLACE FUNCTION change_data(attribute1 VARCHAR, attribute2 INTEGER)
+RETURNS VOID AS $$
+BEGIN
+    EXECUTE 'UPDATE office SET spot_conf = $1 WHERE name = $2'
+    USING attribute2, attribute1;
+END;
+$$ LANGUAGE plpgsql;
