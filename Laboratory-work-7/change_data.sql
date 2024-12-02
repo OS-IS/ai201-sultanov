875ddb79de4d9f837b04f7421d7db76480a543b1
Artem Sultanov <79450613+ArtemSultanov@users.noreply.github.com>
2024-12-02 12:24:40 +0200

    Update change_data.sql

--- Laboratory-work-7/change_data.sql
+++ Laboratory-work-7/change_data.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION change_data(attr1_value TEXT, new_attr2_value TEXT)
 RETURNS VOID AS $$
 BEGIN
-    EXECUTE 'UPDATE office SET name = $2 WHERE address = $1'
+    EXECUTE 'UPDATE office SET name = $2 WHERE size = $1'
     USING new_attr2_value, attr1_value;
 END;
 $$ LANGUAGE plpgsql;
