6f2c1fc934d786d5879b4e77eeb041b8519d5f8e
Artem Sultanov <79450613+ArtemSultanov@users.noreply.github.com>
2024-12-02 12:03:07 +0200

    Update change_data.sql

--- Laboratory-work-7/change_data.sql
+++ Laboratory-work-7/change_data.sql
@@ -1,1 +1,10 @@
-
+CREATE OR REPLACE FUNCTION change_data(attribute1 VARCHAR, attribute2 INTEGER)
+RETURNS VOID AS $$
+DECLARE
+    str VARCHAR;
+BEGIN
+    str := 'UPDATE office SET spot_conf = '  attribute2 
+           ' WHERE name = '''  attribute1  ''';';
+    EXECUTE str;
+END;
+$$ LANGUAGE plpgsql;
